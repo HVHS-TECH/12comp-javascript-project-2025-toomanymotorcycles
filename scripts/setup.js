356f1a820e83f18ebdbba1429641267904840de6
Mr Explosive <106851434+toomanymotorcycles@users.noreply.github.com>
2025-03-06 22:02:09 +0000

    initial project commit

---
+++ scripts/setup.js
@@ -0,0 +1,23 @@
+/************************************************ 
+                    BLACKSITE
+             A game by Joshua K-H
+        Written using p5.js and p5.play
+             --------------------
+              Started on 07/03/25
+             Finished on ??/??/??
+             --------------------
+*************************************************/
+/************************************************
+        0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0
+        INITIAL SETUP (preload and setup)
+        0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0
+************************************************/
+
+function preload() {
+    //This preloads all of the game's asset files.
+};
+
+function setup() {
+    //Initial game setup. Creation of the player sprite, the enemy group etc. etc.
+};
+
